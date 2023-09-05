9fd02d6135eb7198decf25d26e24330cab24d7bc
syrdominik <dominik.syrovy@gmail.com>
2023-09-05 17:22:19 +0200

    Zalozeni souboru a GIT zalohy

---
+++ cviceni_01.py
@@ -0,0 +1,8 @@
+# Toto je první soubor, kde testuji vlastní zápis kódu Python)
+
+print("Ahoj Praho!")
+
+my_age = (35)   #zadání proměnné
+print(f"Je mi {my_age} let.")   #Zadání proměnné do prostého textu.
+
+
