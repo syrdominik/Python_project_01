ceae4a5eab4b264e0a62a3fb0ec385e50ae07aea
syrdominik <dominik.syrovy@gmail.com>
2023-09-06 14:14:04 +0200

    Uprava prvniho cviceni

--- cviceni_01.py
+++ cviceni_01.py
@@ -1,7 +1,14 @@
 # Toto je první soubor, kde testuji vlastní zápis kódu Python)
 
-print("Ahoj Praho!")
+my_name = input("Vaše jméno: ") #Zadání proměnné formou dotazu na vyplnění.
+my_surname = input("Vaše příjmení: ")
+my_age = input("Váš věk: ")   #zadání proměnné
 
-my_age = (35)   #zadání proměnné
+print(f"Ahoj! Jmenuji se {my_name}")    #Zadáná proměnné do prostého textu.
+
 print(f"Je mi {my_age} let.")   #Zadání proměnné do prostého textu.
 
+# STRINGS - zkoušky s textem
+
+print(f"{my_name} {my_surname}")   #Spojení slov dohromady
+print(my_name * 5)
